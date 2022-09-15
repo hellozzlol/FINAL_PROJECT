f02734c719fc65739dede8866eced593f14d0ac6
SOHYEON <hellozzlol62@naver.com>
2022-09-15 17:52:16 +0900

    20220915소현첫번째커밋

---
+++ TogetherPet/src/main/java/com/team/prj/funeral/mapper/FuneralMapper.java
@@ -0,0 +1,10 @@
+package com.team.prj.funeral.mapper;
+
+import java.util.List;
+
+import com.team.prj.funeral.service.FuneralVO;
+
+public interface FuneralMapper {
+
+	public List<FuneralVO> findAll();
+}
