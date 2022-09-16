a5d55408c1c593bf06acb5972815c88cfefb70ba
H2suLee <h2su0712@naver.com>
2022-09-16 18:04:34 +0900

    09.16 18:03

--- TogetherPet/src/main/java/com/team/prj/goods/web/GoodsController.java
+++ TogetherPet/src/main/java/com/team/prj/goods/web/GoodsController.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.team.prj.goods.service.GoodsService;
 
 @Controller
 public class GoodsController {
-	@Autowired 
+	@Autowired
 	private GoodsService dao;
-	
+
 	@GetMapping("/shop")
 	public String goodsSelectAll(Model model) {
 		model.addAttribute("goodsList", dao.goodsSelectAll());
 		return "shop/goodsSelectAll";
+	}
+
+	@RequestMapping("/goods")
+	public String goodsSelectOne() {
+		return "shop/goodsSelectOne";
 	}
 }
