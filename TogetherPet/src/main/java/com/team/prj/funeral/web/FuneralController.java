bef26ca9caa202695a12809666f523f21f0a7a9e
SOHYEON <hellozzlol62@naver.com>
2022-09-18 21:42:56 +0900

    20220918 커밋

--- TogetherPet/src/main/java/com/team/prj/funeral/web/FuneralController.java
+++ TogetherPet/src/main/java/com/team/prj/funeral/web/FuneralController.java
@@ -6,23 +6,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.team.prj.funeral.mapper.FuneralMapper;
+import com.team.prj.funeral.service.FuneralVO;
 
 @Controller
 public class FuneralController {
-	
+
 	@Autowired
-	FuneralMapper dao;
+	private FuneralMapper dao;
+	
 	
 	@RequestMapping("/funeralList")
-	
+
 	public String funeralList(Model model) {
 		model.addAttribute("funeral", dao.findAll());
 		return "funeral/funeralList";
 	}
+
+	/*
+	 * @RequestMapping("/funeralSelect/{funeralNo}") public String
+	 * funeralSelect(FuneralVO vo,Model model) {
+	 * model.addAttribute("funeral",dao.funeralSelect(vo)); return
+	 * "funeral/funeralSelect"; }
+	 */
+	
+	@RequestMapping("/funeral")
+	public String funeralSelect(FuneralVO vo,Model model) {
+		model.addAttribute("funeral",dao.funeralSelect(vo));
+		return  "funeral/funeralSelect";
+	}
 	
 	
-	
-	
-	
-	
+
 }
