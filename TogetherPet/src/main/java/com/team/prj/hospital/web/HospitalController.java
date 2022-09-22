8865ed878f660e8ec62f84c411863e5df8b49baf
H2suLee <h2su0712@naver.com>
2022-09-22 18:10:24 +0900

    0922 희수학원

--- TogetherPet/src/main/java/com/team/prj/hospital/web/HospitalController.java
+++ TogetherPet/src/main/java/com/team/prj/hospital/web/HospitalController.java
@@ -1,7 +1,7 @@
 package com.team.prj.hospital.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.team.prj.hospital.service.HospitalService;
 import com.team.prj.hospital.service.HospitalVO;
 
-
 @Controller
 public class HospitalController {
+	
+	@Autowired
+	private HospitalService dao;
 
 	// 개인 회원 => 병원 스크랩 리스트
 	@GetMapping("/usersHospitalList")
@@ -19,18 +21,18 @@ public class HospitalController {
 		model.addAttribute("hospitalList", dao.hospitalSelectList());
 		return "users/usersHospitalList";
 	}
-	
+
 	@RequestMapping("/hospitalList")
-	 public String hospitalList(Model model) {
+	public String hospitalList(Model model) {
 		model.addAttribute("hospital", dao.hospitalSelectList());
 		return "hospital/hospitalList";
-		
+
 	}
-	
+
 	@RequestMapping("/hospital")
 	public String hospitalSelect(HospitalVO vo, Model model) {
-		model.addAttribute("hospital",dao.hospitalSelect(vo));
-		dao.hospitalHitUpdate(vo);//조회수 증가
+		model.addAttribute("hospital", dao.hospitalSelect(vo));
+		dao.hospitalHitUpdate(vo);// 조회수 증가
 		return "hospital/hospitalSelect";
 	}
 
