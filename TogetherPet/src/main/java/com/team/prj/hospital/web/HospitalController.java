875d8e4dd2f34bab567eb024e421d50d030c7d83
SOHYEON <hellozzlol62@naver.com>
2022-09-22 17:55:27 +0900

    20220922오늘두번째커밋

--- TogetherPet/src/main/java/com/team/prj/hospital/web/HospitalController.java
+++ TogetherPet/src/main/java/com/team/prj/hospital/web/HospitalController.java
@@ -1,7 +1,7 @@
 package com.team.prj.hospital.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +12,8 @@ import com.team.prj.hospital.service.HospitalVO;
 
 @Controller
 public class HospitalController {
+	@Autowired
+	private HospitalService dao;
 
 	// 개인 회원 => 병원 스크랩 리스트
 	@GetMapping("/usersHospitalList")
