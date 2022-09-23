9ebe80b7c22b10c75f804fe60ee5722cfb7d21b5
hee <sunhee1600@gmail.com>
2022-09-23 17:54:09 +0900

    220923 hee

--- TogetherPet/src/main/java/com/team/prj/seller/web/SellerController.java
+++ TogetherPet/src/main/java/com/team/prj/seller/web/SellerController.java
@@ -59,11 +59,39 @@ public class SellerController {
 		model.addAttribute("sellerList", seller.sellerUpdate(vo));
 		return "redirect:sellerSelect";
 	}
-	
+
 	// 판매 상품 등록 폼 호출
 	@RequestMapping("/seller/sellerGIForm")
 	public String sellerGoodsInsert() {
 		return "seller/sellerGIForm";
+	}
+
+	// 판매 상품 조회
+	@RequestMapping("/seller/sellerGoodsList")
+	public String sellerGoodsList(Model model) {
+		model.addAttribute("sellerList", seller.sellerSelectList());
+		return "seller/sellerGoodsList";
+	}
+
+	// 배송 관리 페이지
+	@RequestMapping("/seller/sellerDeliList")
+	public String sellerDeliList(Model model) {
+		model.addAttribute("sellerList", seller.sellerSelectList());
+		return "seller/sellerDeliList";
+	}
+
+	// 취소/반품 관리 페이지
+	@RequestMapping("/seller/sellerCanList")
+	public String sellerCanList(Model model) {
+		model.addAttribute("sellerList", seller.sellerSelectList());
+		return "seller/sellerCanList";
+	}
+
+	// 판매완료 상품 관리 페이지
+	@RequestMapping("/seller/sellerDoneList")
+	public String sellerDoneList(Model model) {
+		model.addAttribute("sellerList", seller.sellerSelectList());
+		return "seller/sellerDoneList";
 	}
 
 }
