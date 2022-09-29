a470d5e10c7e7bfa42d093ab717ff783849a6a3b
SOHYEON <hellozzlol62@naver.com>
2022-09-29 23:29:35 +0900

    20220929 댓글리스트 띄우는거 완료 학원에서 입력 삭제 구현

--- TogetherPet/src/main/java/com/team/prj/comment/service/CommentVO.java
+++ TogetherPet/src/main/java/com/team/prj/comment/service/CommentVO.java
@@ -1,6 +1,12 @@
 package com.team.prj.comment.service;
 
+
+
+
+
 import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -8,6 +14,7 @@ import lombok.Data;
 public class CommentVO {
 	private int commentNo; // 댓글번호
 	private String content; // 댓글내용
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date dt; // 댓글날짜
 	private int boardNo; // 게시글번호
 	private int userNo; // 유저번호
