f12ee3f700d1910994a22d66bc082e19de4eabc2
parkjihyee <jhyee1021@gmail.com>
2022-09-29 18:04:44 +0900

    2022 0929

--- TogetherPet/src/main/java/com/team/prj/join/service/JoinServiceImpl.java
+++ TogetherPet/src/main/java/com/team/prj/join/service/JoinServiceImpl.java
@@ -18,51 +18,23 @@ public class JoinServiceImpl implements JoinService {
 	private JoinMapper jm;
 
 	
-
-	@Override
-	public List<UsersVO> usersList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String stateUpdate(UsersVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	@Override
-	public List<TutorVO> tutorList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String stateUpdate(TutorVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	@Override
-	public List<SellerVO> sellerList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String stateUpdate(SellerVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-	// id 중복체크
+	// 일반회원 id 중복체크
 	@Override
 	public int idCheck(String id){
 		return jm.idCheck(id);
 	}
+	
+	// 튜터회원 id 중복체크
+	@Override
+	public int tCheck(String id){
+		return jm.tCheck(id);
+	}
+
+	// 판매자회원 id 중복체크
+	@Override 
+	public int sCheck(String id) {
+		return jm.sCheck(id);
+	}	
 
 	// 닉네임 중복체크
 	@Override
@@ -74,14 +46,50 @@ public class JoinServiceImpl implements JoinService {
 	// 일반회원 등록
 	@Override
 	public int usersInsert(UsersVO vo) {
-		// 비밀번호 암호화
+		// 비밀번호 암호화 (DB)
 		BCryptPasswordEncoder Pencoder = new BCryptPasswordEncoder();
 		String result = Pencoder.encode(vo.getPassword());
 		vo.setPassword(result);
 				
 		return jm.usersInsert(vo);
 	}
+	
+	// 튜터회원 등록
+	@Override
+	public int tutorInsert(TutorVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// 판매자회원 등록
+	@Override
+	public int sellerInsert(SellerVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	
+	// 회원 상태 업데이트
+	@Override
+	public String stateUpdate(UsersVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// 튜터 상태 업데이트
+	@Override
+	public String stateUpdate(TutorVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// 판매자 상태 업데이트
+	@Override
+	public String stateUpdate(SellerVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
