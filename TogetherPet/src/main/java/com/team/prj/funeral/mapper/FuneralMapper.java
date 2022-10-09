6286d75427a5a70f73d136112c8e70a21f31ac68
SOHYEON <hellozzlol62@naver.com>
2022-10-09 15:36:14 +0900

    20221009 브런치 새로생성

--- TogetherPet/src/main/java/com/team/prj/funeral/mapper/FuneralMapper.java
+++ TogetherPet/src/main/java/com/team/prj/funeral/mapper/FuneralMapper.java
@@ -3,6 +3,7 @@ package com.team.prj.funeral.mapper;
 import java.util.List;
 
 import com.team.prj.funeral.service.FuneralVO;
+import com.team.prj.review.service.ReviewVO;
 import com.team.prj.scrap.service.ScrapVO;
 
 
@@ -18,5 +19,16 @@ public interface FuneralMapper {
 	
 		//소현스크랩기능
 		int scrapInsert(ScrapVO vo);//20221007소현추가
+		
+		//소현 리뷰기능 20221008
+				List<ReviewVO> reviewSelectList(ReviewVO vo);
+				
+				ReviewVO reviewSelect(ReviewVO vo);
+				
+				int reviewInsert(ReviewVO vo);
+				
+				int reviewUpdate(ReviewVO vo);
+				
+				int reviewDelete(ReviewVO vo);
 	
 }
