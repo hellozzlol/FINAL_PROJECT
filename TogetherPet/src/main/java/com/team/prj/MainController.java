b5b2f082762dd2fab3ede46eaee769fffd95aece
SOHYEON <hellozzlol62@naver.com>
2022-10-12 17:43:55 +0900

    20221012 403페이지수정 리뷰하트 수정

--- TogetherPet/src/main/java/com/team/prj/MainController.java
+++ TogetherPet/src/main/java/com/team/prj/MainController.java
@@ -6,11 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.team.prj.goods.service.GoodsService;
+import com.team.prj.hospital.service.HospitalService;
 
 @Controller
 public class MainController {
 	@Autowired
 	private GoodsService goods;
+	
+	@Autowired
+	private HospitalService hospital;
+	
+	
 
 	@GetMapping("/index")
 	public String index(String key, Model model) {
@@ -19,6 +25,7 @@ public class MainController {
 			key = "1";
 		}
 		model.addAttribute("goodsList", goods.goodsSelectAll(key));
+		model.addAttribute("hospital",hospital.hospitalSelectList(key));//소현 20221012추가 
 		model.addAttribute("reviewCount", goods.reviewCount());
 		
 		return "index";
@@ -28,4 +35,11 @@ public class MainController {
 	public String error403() {
 		return "403";
 	}
+	
+	
+	
+	
+	
+	
+	
 }
