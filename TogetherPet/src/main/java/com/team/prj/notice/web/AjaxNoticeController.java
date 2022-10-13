96dbd10bee5f555041d47ce2a02a27b1fcb74507
sunhe <sunhe@DESKTOP-HG6LG5S>
2022-10-14 00:05:00 +0900

    221014 sunhee

--- TogetherPet/src/main/java/com/team/prj/notice/web/AjaxNoticeController.java
+++ TogetherPet/src/main/java/com/team/prj/notice/web/AjaxNoticeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.PageHelper;
@@ -20,23 +21,23 @@ import com.team.prj.users.service.UsersVO;
 public class AjaxNoticeController {
 	@Autowired
 	NoticeService notice;
-	
+
 	// 알림 페이지
 	@RequestMapping("/noticeList")
 	public List<NoticeVO> noticeList(NoticeVO vo, Model model, HttpSession session) {
-		//UsersVO u = (UsersVO) session.getAttribute("user");
-		//vo.setUserNo(u.getUserNo());
+		// UsersVO u = (UsersVO) session.getAttribute("user");
+		// vo.setUserNo(u.getUserNo());
 		System.out.println("==========" + vo.toString());
 		return notice.noticeSelectList(vo);
 	}
-	
+
 	// 알림 카운트
 	@RequestMapping("/ajaxNoticeCount")
 	public int ajaxNoticeCount(NoticeVO vo) {
 		int cnt = notice.noticeCount(vo);
 		return cnt;
 	}
-	
+
 	// 알림 등록(0번 댓글, 1번 일정, 2번 문의, 3번 배송)
 	@RequestMapping("/ajaxNoticeInsert")
 	public int ajaxNoticeInsert(NoticeVO vo) {
@@ -44,12 +45,14 @@ public class AjaxNoticeController {
 		notice.noticeInsert(vo);
 		return noticeNo;
 	}
-	
+
 	// 알림 상태 수정(읽은시간)
-//	@RequestMapping("/ajaxNoticeUpdate")
-//	public int ajaxNoticeUpdate(NoticeVO vo) {
-//		int cnt = notice.noticeUpdate(vo);
-//		return cnt;
-//	}
+	@RequestMapping("/noticeUpdate")
+	@ResponseBody
+	public int noticeUpdate(NoticeVO nvo) {
+		System.out.println("===============");
+		System.out.println(nvo.getNoticeNo());
+		return notice.noticeUpdate(nvo);
+	}
 
 }
