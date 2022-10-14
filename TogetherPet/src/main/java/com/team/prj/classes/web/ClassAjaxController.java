62dc98cd9ee6987cf006e3f31d3913cd9bebe6b9
dhwlals24 <dhwlals24@gmail.com>
2022-10-14 10:50:20 +0900

    경로수정

--- TogetherPet/src/main/java/com/team/prj/classes/web/ClassAjaxController.java
+++ TogetherPet/src/main/java/com/team/prj/classes/web/ClassAjaxController.java
@@ -265,7 +265,7 @@ public class ClassAjaxController {
 	@GetMapping("/display")
 	public ResponseEntity<byte[]> getImage(String fileName){
 		//File file = new File("C:\\Users\\admin\\git\\FINAL_PROJECT\\TogetherPet\\src\\main\\resources\\Temp\\" + fileName);
-		//테스트용
+		//테스트
 		File file = new File("/home/Temp", fileName);
 		ResponseEntity<byte[]> result = null;
 		
